396d425e0af6fb986a47c413b19cd793e4120e21
rusky65 <->
2018-07-13 11:05:36 +0200

    #3: Creating MenuItems controller.   - Manage authorized user's view

---
+++ WebAppRestaurant/Controllers/MenuItemsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppRestaurant.Models;
+
+namespace WebAppRestaurant.Controllers
+{
+    public class MenuItemsController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        #region Open actions parts
+        // GET: MenuItems
+        public ActionResult Index()
+        {
+            return View(db.MenuItems.ToList());
+        }
+
+        // GET: MenuItems/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+        #endregion Open actions parts
+
+        #region Only for authorized users
+        // GET: MenuItems/Create
+        /// <summary>
+        /// Only authorized user can use this controller.
+        /// </summary>
+        [Authorize]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MenuItems/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Price")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.MenuItems.Add(menuItem);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Edit/5
+        [Authorize]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Price")] MenuItem menuItem)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(menuItem).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(menuItem);
+        }
+
+        // GET: MenuItems/Delete/5
+        [Authorize]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            MenuItem menuItem = db.MenuItems.Find(id);
+            if (menuItem == null)
+            {
+                return HttpNotFound();
+            }
+            return View(menuItem);
+        }
+
+        // POST: MenuItems/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            MenuItem menuItem = db.MenuItems.Find(id);
+            db.MenuItems.Remove(menuItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        #endregion Only for authorized users
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
