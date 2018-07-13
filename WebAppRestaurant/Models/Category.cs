614e0cbf37adab007e3eb091b6d2e812a9ac733b
rusky65 <->
2018-07-13 11:57:47 +0200

    #3, #4: New class and table for Category. New property in MenuItem class.

---
+++ WebAppRestaurant/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppRestaurant.Models {
+
+    public class Category {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(200)]
+        public string Name { get; set; }
+    }
+}
--- WebAppRestaurant/Models/MenuItem.cs
+++ WebAppRestaurant/Models/MenuItem.cs
@@ -13,5 +13,6 @@ namespace WebAppRestaurant.Models {
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+        public Category Category { get; set; }
     }
 }
