277624c47ca77f04472493576c6c3b7ea11ef0c7
rusky65 <->
2018-07-19 23:06:31 +0200

    #4: Insert test datas into database with the Seed() function.

--- WebAppRestaurant/Migrations/Configuration.cs
+++ WebAppRestaurant/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace WebAppRestaurant.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -13,8 +14,80 @@ namespace WebAppRestaurant.Migrations
             ContextKey = "WebAppRestaurant.Models.ApplicationDbContext";
         }
 
+        /// <summary>
+        /// This process run always after the "update-database" migration command.
+        /// </summary>
+        /// <param name="context"></param>
         protected override void Seed(WebAppRestaurant.Models.ApplicationDbContext context)
         {
+            // Insert the categories
+            var category1 = new Category { Name = "Levesek" };
+            var category2 = new Category { Name = "Hideg elõételek" };
+            var category3 = new Category { Name = "Meleg elõételek" };
+
+            context.Categories.AddOrUpdate(category1);
+            context.Categories.AddOrUpdate(category2);
+            context.Categories.AddOrUpdate(category3);
+
+            // Insert the menus
+            //            Id Name    Description Price   Category_Id
+            //1   Tengeri hal trió Atlanti lazactatár, pácolt lazacfilé és tonhal lazackaviárral   7500    2
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                    Name = "Tengeri hal trió Atlanti lazactatár",
+                    Description = "pácolt lazacfilé és tonhal lazackaviárral",
+                    Price = 7500,
+                    Category = category2
+            });
+
+            //2   Füstölt pisztráng Gundel módra  Burgonyasaláta  4500    2
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                Name = "Füstölt pisztráng Gundel módra",
+                Description = "Burgonyasaláta",
+                Price = 4500,
+                Category = category2
+            });
+
+            //3   Borjúesszencia Zöldséges gyöngytyúk galuska    4500    1
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                Name = "Borjúesszencia",
+                Description = "Zöldséges gyöngytyúk galuska",
+                Price = 4500,
+                Category = category1
+            });
+
+            //4   Gundel saláta 1910  Spárga, paradicsom, uborka, sült paprika, zöldbab, gomba, jégsaláta 3500    2
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                Name = "Gundel saláta 1910",
+                Description = "Spárga, paradicsom, uborka, sült paprika, zöldbab, gomba, jégsaláta",
+                Price = 3500,
+                Category = category2
+            });
+
+            //5   Szarvasgomba cappuccino Finom, nagyon.  4500    1
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                Name = "Szarvasgomba cappuccino",
+                Description = "Finom, nagyon. :)",
+                Price = 4500,
+                Category = category1
+            });
+
+            //6   Hirtelen sült fogasderék illatos erdei gombákkal    meleg   4500    3
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                Name = "Hirtelen sült fogasderék illatos erdei gombákkal",
+                Description = "meleg",
+                Price = 4500,
+                Category = category3
+            });
+
+            //7   Szárított érlelt bélszín carpaccio  Öreg Trappista sajt, keserû levelek 5000    2
+            context.MenuItems.AddOrUpdate(new MenuItem() {
+                Name = "Szárított érlelt bélszín carpaccio",
+                Description = "Öreg Trappista sajt, keserû levelek",
+                Price = 5000,
+                Category = category2
+            });
+
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
